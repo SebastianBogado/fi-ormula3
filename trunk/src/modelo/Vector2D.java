f5cc28275ae925befb2f1db6882f6298aa4dbfb0
mat1204.torres <mat1204.torres@9420b9a2-6e37-d7f8-2141-93c9749b6bdd>
2010-11-11 15:54:38 +0000

    ...

--- trunk/src/modelo/Vector2D.java
+++ trunk/src/modelo/Vector2D.java
@@ -4,20 +4,25 @@ import java.awt.geom.Point2D;
 
 public class Vector2D {
 	protected Point2D.Double vector;
-	
-	public Vector2D(double x, double y){
+
+	public Vector2D(double x, double y) {
+		vector = new Point2D.Double();
 		vector.setLocation(x, y);
 	}
-	public double x(){
+
+	public double x() {
 		return vector.getX();
 	}
-	public double y(){
+
+	public double y() {
 		return vector.getY();
 	}
-	public void x(double x){
+
+	public void x(double x) {
 		vector.setLocation(x, vector.getY());
 	}
-	public void y(double y){
+
+	public void y(double y) {
 		vector.setLocation(vector.getX(), y);
 	}
 

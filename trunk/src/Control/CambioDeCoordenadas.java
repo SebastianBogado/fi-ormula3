f0e756e37e21f23330aca6e3d65ad4ff646e438d
mat1204.torres <mat1204.torres@9420b9a2-6e37-d7f8-2141-93c9749b6bdd>
2010-12-01 11:13:15 +0000

    Clase para la tranformacion de coordenadas para el modelo con la vista

--- trunk/src/Control/CambioDeCoordenadas.java
+++ trunk/src/Control/CambioDeCoordenadas.java
@@ -1,22 +1,43 @@
 package Control;
 
 import modelo.Entorno;
+import modelo.servicio.Posicion;
+import Vista.PosicionDiscreta;
 
 public class CambioDeCoordenadas {
 
 	private static Entorno entorno;
+	private static Posicion posicionAuto;
 
 	public static void setEntorno(Entorno ent) {
 		entorno = ent;
+		posicionAuto = entorno.getPosicionAuto();
+
 	}
 
-	public static long realizarCambio(long x) {
+	public static long realizarCambio(long y) {
 
-		long nuevaPosicion = 0, posicionActual = x;
+		long nuevaPosicion = 0;
 
-		nuevaPosicion = posicionActual - entorno.getPosicionAutoDiscretaEnX();
+		nuevaPosicion = y - ((long) posicionAuto.y()) + 645 / 10;// 645 es
+		// el
+		// largo
+		// de la
+		// ventana
 
 		return nuevaPosicion;
 
+	}
+
+	public static PosicionDiscreta realizarCambio(Posicion posicionObstaculo) {
+
+		PosicionDiscreta nuevaPosicion = new PosicionDiscreta();
+		nuevaPosicion.setY((int) ((long) posicionObstaculo.y() - posicionAuto
+				.y()) + 645);
+
+		nuevaPosicion.setX((int) ((long) posicionObstaculo.x() - posicionAuto
+				.x()));
+
+		return nuevaPosicion;
 	}
 }

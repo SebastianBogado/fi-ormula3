aa6179efed1debeaff32dbc2dbcc3699e3bff44b
mat1204.torres <mat1204.torres@9420b9a2-6e37-d7f8-2141-93c9749b6bdd>
2010-12-01 11:18:44 +0000

    Metodo agreagdo en las clases Ripio y Tierra

--- src/modelo/Ripio.java
+++ src/modelo/Ripio.java
@@ -1,46 +1,54 @@
 package modelo;
 
+import modelo.excepciones.ExcepcionLimitesIncorrectosEnElTerreno;
+
 import org.jdom.Attribute;
 import org.jdom.Element;
 
-import modelo.excepciones.ExcepcionLimitesIncorrectosEnElTerreno;
-
 public class Ripio extends Terreno {
 
-	public Ripio(int principio, int fin) throws ExcepcionLimitesIncorrectosEnElTerreno {
+	public Ripio(int principio, int fin)
+			throws ExcepcionLimitesIncorrectosEnElTerreno {
 		super(principio, fin);
 	}
 
 	/*
 	 * crea un nueva instancia de ripio a partir de un elemento xml
 	 */
-	public Ripio( Element element) throws ExcepcionLimitesIncorrectosEnElTerreno
-	{
+	public Ripio(Element element) throws ExcepcionLimitesIncorrectosEnElTerreno {
 		String principio = element.getAttributeValue("principio");
 		String fin = element.getAttributeValue("fin");
-		
-		this.principio=Integer.parseInt(principio);
-		this.fin=Integer.parseInt(fin);
-		if (this.fin < this.principio) { throw new ExcepcionLimitesIncorrectosEnElTerreno(); }
+
+		this.principio = Integer.parseInt(principio);
+		this.fin = Integer.parseInt(fin);
+		if (this.fin < this.principio) {
+			throw new ExcepcionLimitesIncorrectosEnElTerreno();
+		}
 	}
-	
+
 	@Override
 	public double desgastar(Neumaticos neumaticos) {
 		return neumaticos.desgastePorRipio();
 	}
-	
-public Element serializarXML(){
-		
-		Element element= new Element("Ripio");
-		
-		Attribute principio = new Attribute ("principio",String.valueOf(this.principio()));
-		Attribute fin = new Attribute ("fin",String.valueOf(this.fin()));
-		
+
+	@Override
+	public Element serializarXML() {
+
+		Element element = new Element("Ripio");
+
+		Attribute principio = new Attribute("principio", String.valueOf(this
+				.principio()));
+		Attribute fin = new Attribute("fin", String.valueOf(this.fin()));
+
 		element.getAttributes().add(principio);
 		element.getAttributes().add(fin);
-		
+
 		return element;
 	}
 
+	@Override
+	public String tipoTerreno() {
+		return Ripio;
+	}
 
 }

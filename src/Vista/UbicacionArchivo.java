5cf90fd21e2e903bdc9bdf76a932a5f4653ca46f
sebastian.j.bogado <sebastian.j.bogado@9420b9a2-6e37-d7f8-2141-93c9749b6bdd>
2010-12-02 18:23:33 +0000

    Agregado: ahora la ventana apunta al directorio por defecto de las pistas

--- src/Vista/UbicacionArchivo.java
+++ src/Vista/UbicacionArchivo.java
@@ -3,14 +3,16 @@ package Vista;
 public class UbicacionArchivo {
 
 	public static final String ImagenBandera = "Docs/Bandera.JPG";
-	public static String ImagenPozo = "Docs/Pozo.jpg";// Falta subir la Imagen
-	public static String ImagenLomaDeBurro = "Docs/LomaDeBurro.jpg";// Falta
+	public static final String ImagenPozo = "Docs/Pozo.jpg";// Falta subir la Imagen
+	public static final String ImagenLomaDeBurro = "Docs/LomaDeBurro.jpg";// Falta
 	// subir la
 	// imagen
-	public static String ImagenAutomovil = "Docs/Automovil.jpg";
+	public static final String ImagenAutomovil = "Docs/Automovil.jpg";
 
-	public static String ImagenRipio = "Docs/Ripio.jpg";
-	public static String ImagenAsfalto = "Docs/Asfalto.jpg";
-	public static String ImagenTierra = "Docs/Tierra.jpg";
+	public static final String ImagenRipio = "Docs/Ripio.jpg";
+	public static final String ImagenAsfalto = "Docs/Asfalto.jpg";
+	public static final String ImagenTierra = "Docs/Tierra.jpg";
+	
+	public static final String Pistas = "pistas/";
 
 }

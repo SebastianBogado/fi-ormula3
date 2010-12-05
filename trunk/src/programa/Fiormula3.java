f3b47ff9064f67d5dd4b1edbc0ccccdc44c099f4
sebastian.j.bogado <sebastian.j.bogado@9420b9a2-6e37-d7f8-2141-93c9749b6bdd>
2010-12-05 20:39:35 +0000

    Actualizadas las referencias a los archivos del disco. Main limpio de comentarios

--- trunk/src/programa/Fiormula3.java
+++ trunk/src/programa/Fiormula3.java
@@ -10,13 +10,6 @@ public class Fiormula3 {
 
 	public static void main(String[] args) {
 		new MenuPrincipal();
-
-		// MetadataAuto metaAuto = new MetadataAuto("Mess10", "Lin64");
-		// Automovil auto = FabricaDeAutos.instanciarAuto(metaAuto);
-		// Pista pista = new Pista("\\pistas\\Default.xml");
-		//
-		// new ControladorDeCarrera(auto, pista);
-
 	}
 
 }
--- trunk/src/vista/UbicacionArchivo.java
+++ trunk/src/vista/UbicacionArchivo.java
@@ -2,13 +2,13 @@ package vista;
 
 public class UbicacionArchivo {
 
-	public static final String ImagenBandera = "Docs/Bandera.JPG";
-	public static final String ImagenPozo = "Docs/Pozo.jpg";
-	public static final String ImagenLomaDeBurro = "Docs/LomaDeBurro.jpg";
-	public static final String ImagenAutomovil = "Docs/Automovil.jpg";
-	public static final String ImagenRipio = "Docs/Ripio.jpg";
-	public static final String ImagenAsfalto = "Docs/Asfalto.jpg";
-	public static final String ImagenTierra = "Docs/Tierra.jpg";
+	public static final String ImagenBandera = "res/Bandera.JPG";
+	public static final String ImagenPozo = "res/Pozo.jpg";
+	public static final String ImagenLomaDeBurro = "res/LomaDeBurro.jpg";
+	public static final String ImagenAutomovil = "res/Automovil.jpg";
+	public static final String ImagenRipio = "res/Ripio.jpg";
+	public static final String ImagenAsfalto = "res/Asfalto.jpg";
+	public static final String ImagenTierra = "res/Tierra.jpg";
 	public static final String Pistas = "pistas/";
 
 }
